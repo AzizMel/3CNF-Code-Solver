e5634d4cfd8803dbb4ed3ed03907407c782680a3
Aziz Mel <eng.aziz.mel@gmail.com>
2025-09-13 06:02:38 +0330

    working on solver

---
+++ finding_best_solver.py
@@ -0,0 +1,43 @@
+import json
+import devTools
+import formulas_data
+from Solvers.dpll_solver import DPLLSolver
+from Solvers.walk_sat_solver import WalkSatSolver
+from Solvers.brute_force_solver import BruteForceSolver
+
+results = {}
+
+for index in formulas_data.formulas:
+    formula_to_solve = formulas_data.formulas[index]
+    dpll_solver = DPLLSolver(formula_to_solve)
+    walk_sat_solver = WalkSatSolver(formula_to_solve)
+    brute_force_solver = BruteForceSolver(formula_to_solve)
+
+    brute_force_solver_result = devTools.run_function_with_timing(brute_force_solver.solve)
+    walk_sat_solver_result = devTools.run_function_with_timing(walk_sat_solver.solve)
+    dpll_solver_result = devTools.run_function_with_timing(dpll_solver.solve)
+
+    results[index] = {
+        "formula_to_solve": formula_to_solve,
+        "len_formula": len(formula_to_solve),
+
+        "dpll_solver": {
+            "time": dpll_solver_result[0][1],
+            "solved": dpll_solver_result[0][0] is not None,
+            "solution": dpll_solver_result[0][0]
+        },
+        "brute_force_solver": {
+            "time": brute_force_solver_result[0][1],
+            "solved": brute_force_solver_result[0][0] is not None,
+            "solution": brute_force_solver_result[0][0]
+        },
+        "walk_sat_solver": {
+            "time": walk_sat_solver_result[0][1],
+            "solved": walk_sat_solver_result[0][0] is not None,
+            "solution": walk_sat_solver_result[0][0]
+        }
+    }
+
+# Save results to a JSON file
+with open("solver_results.json", "w") as f:
+    json.dump(results, f, indent=4)
